39dec3244f367e925d1f44689b83d614349f4b74
herwebcode <bashiratajoke247@gmail.com>
2025-05-03 14:39:40 +0100

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,22 @@
+num1 = 2
+num2 = 5
+operator = input("Enter an operator (+, -, *, /): ")
+
+# Now use if-elif-else to perform the calculation
+if operator == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operator == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operator == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid operator. Please enter +, -, *, or /.")
